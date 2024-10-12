1ee1de5473c10f718c73c6032af3f6d79cb8821d
Rovinya Wijerama <rovinyawijeramaofficial@gmail.com>
2024-10-12 09:55:26 +0530

    updated

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import { getFeaturedEvents } from "../dummy-data";
 import EventList from "../components/events/event-list";
+import EventsSearch from "../components/events/events-search";
+import { Fragment } from "react";
 
 function HomePage(){
     const featuredEvents= getFeaturedEvents();
+    
     return(
-        <div>
+        <Fragment>
+            <EventsSearch/>
             <EventList items={featuredEvents}/>
-        </div>
+        </Fragment>   
     );
 }
 
